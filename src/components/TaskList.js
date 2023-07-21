a79012ababde498f3c69cd1499c3201a83c85230
samuel <samuelkariuki1@student.moringaschool.com>
2023-07-21 08:31:23 +0300

    form lab

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, setTasks }) {
+  const handleDelete = (task) => {
+    // Filter out the task with the given text and category
+    const updatedTasks = tasks.filter(
+      (t) => t.text !== task.text && t.category !== task.category
+    );
+    // Update the tasks
+    // You can also use a state management library like Redux for this
+    // but for this example, we're just using useState in App to update tasks
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task, index) => (
+        <Task
+          key={index}
+          text={task.text}
+          category={task.category}
+          onDelete={() => handleDelete(task)}
+        />
+      ))}
     </div>
   );
 }
